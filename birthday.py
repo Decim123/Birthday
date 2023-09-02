a1e37dba0537d179c3ec7de31eb2189a3fb077a6
Decim123 <aleksandrtuman@gmail.com>
2023-09-02 18:48:15 +0300

    Fix

--- birthday.py
+++ birthday.py
@@ -1,8 +1,5 @@
 import datetime
 from datetime import datetime
 now = datetime.now()
-my_birthday = datetime(now.year, 4, 20)
-if now > my_birthday:
-    my_birthday = datetime((now.year) + 1, 4, 20)
-difference =  my_birthday - now
-print(my_birthday - now)
+my_birthday = datetime(2005, 4, 20)
+print(now - my_birthday)
